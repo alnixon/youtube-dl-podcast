53b8742686d9af97c1c917a31301a946f6e5c4a1
Al <alnixon@users.noreply.github.com>
2023-01-11 13:44:59 +0000

    Switch to using yt-dlp instead of youtube-dl

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-# Download oldest first
-#--playlist-reverse
+# Download only the most recent 30
+#--playlist-end 30
 
 # Name the files by their title, in data directory
-#--o "./data/%(title)s.%(ext)s"
+#--output "./data/%(title)s.%(ext)s"
 
 # Maintain a list of downloaded files, don't re-download
-#--download-archive downloaded.txt 
+#--download-archive downloaded.txt
 
 # Don't overwrite files
 #--no-overwrites
 
-# Convert to audio only
-#--extract-audio 
-
-# As mp3
-#--audio-format mp3 
-
-# Best audio quality
-#--audio-quality 0
-
-# Prefer ffmpeg over avconv for running the postprocessors (default)
-#--prefer-ffmpeg
+# Format best audio quality
+#-f 'bestaudio[ext=m4a]'
 
 # Write video description to a .description file
-#--write-description 
+#--write-description
 
 # Write video metadata to a .info.json file
 #--write-info-json
@@ -39,18 +30,19 @@
 # Read list of channels from file
 #-a channels.txt
 
+# Add verbose info to the log
+#-v
+
 logDate=$(date +%Y%m%d)
 feedDate=$(date +%Y%m%d -d "-1 month")
-path="/var/www/YTDL"
+configpath="/var/www/YTDL"
+datapath="/var/www/YTDL/data"
 
-# Update youtube-dl
-youtube-dl -U
+# Update yt-dlp
+yt-dlp --update >> "$datapath/log-$logDate.log"
 
-# Download and convert files
-youtube-dl --playlist-reverse --o "$path/data/%(id)s.%(ext)s" --download-archive "$path/downloaded.txt" --no-overwrites --extract-audio --audio-format mp3 --audio-quality 0 --prefer-ffmpeg --write-description --write-info-json --write-thumbnail --dateafter $feedDate -a "$path/channels.txt" -v >> "$path/data/log-$logDate.log"
+# Download and convert files with progress bar for monitoring
+yt-dlp --playlist-end 30 --output "$datapath/%(id)s.%(ext)s" --download-archive "$configpath/downloaded.txt" --no-overwrites -f 'bestaudio[ext=m4a]' --write-description --write-info-json --write-thumbnail --dateafter $feedDate -a "$configpath/channels.txt" -v >> "$datapath/log-$logDate.log"
 
 # Remove files older than 60 days
-find $path/data/ -mtime +60 -type f -delete >> "$path/data/log-$logDate.log"
-
-
-
+find $datapath/ -mtime +60 -type f -delete >> "$datapath/log-$logDate.log"
