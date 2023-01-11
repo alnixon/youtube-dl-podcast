84d760a8ecb29fef4af76c4249f5d57c8ba8e205
Al <alnixon@users.noreply.github.com>
2023-01-11 13:49:21 +0000

    Switch to using yt-dlp for powershell too

--- youtube-dl.ps1
+++ youtube-dl.ps1
@@ -1,29 +1,20 @@
-# Download oldest first
-#--playlist-reverse
+# Download only the most recent 30
+#--playlist-end 30
 
 # Name the files by their title, in data directory
-#--o "./data/%(title)s.%(ext)s"
+#--output "./data/%(title)s.%(ext)s"
 
 # Maintain a list of downloaded files, don't re-download
-#--download-archive downloaded.txt 
+#--download-archive downloaded.txt
 
 # Don't overwrite files
 #--no-overwrites
 
-# Convert to audio only
-#--extract-audio 
-
-# As mp3
-#--audio-format mp3 
-
-# Best audio quality
-#--audio-quality 0
-
-# Prefer ffmpeg over avconv for running the postprocessors (default)
-#--prefer-ffmpeg
+# Format best audio quality
+#-f 'bestaudio[ext=m4a]'
 
 # Write video description to a .description file
-#--write-description 
+#--write-description
 
 # Write video metadata to a .info.json file
 #--write-info-json
@@ -37,20 +28,23 @@
 # Read list of channels from file
 #-a channels.txt
 
+# Add verbose info to the log
+#-v
 
 $logDate = Get-Date -Format "yyyyMMdd"
 $feedDate = Get-Date (Get-Date).AddMonths(-1) -Format "yyyyMMdd"
-$path = "C:\\www\YTDL"
+$configpath = "C:\\www\YTDL"
+$datapath = "C:\\www\YTDL\data"
 
-if(-not (Test-Path $path/data/log-$logDate.log)){
-    New-Item "$path/data/log-$logDate.log" -Force
+if(-not (Test-Path $datapath/log-$logDate.log)){
+    New-Item "$datapath/log-$logDate.log" -Force
 }
 
 # Update youtube-dl
-youtube-dl -U
+yt-dlp --update | Out-File "$datapath/log-$logDate.log" -Append
 
 # Download and convert files
-youtube-dl --playlist-reverse --o "$path/data/%(id)s.%(ext)s" --download-archive "$path/downloaded.txt" --no-overwrites --extract-audio --audio-format mp3 --audio-quality 0 --prefer-ffmpeg --write-description --write-info-json --write-thumbnail --dateafter $feedDate -a "$path/channels.txt" -v | Out-File "$path/data/log-$logDate.log" -Append
+yt-dlp --playlist-end 30 --output "$datapath/%(id)s.%(ext)s" --download-archive "$configpath/downloaded.txt" --no-overwrites -f 'bestaudio[ext=m4a]' --write-description --write-info-json --write-thumbnail --dateafter $feedDate -a "$configpath/channels.txt" -v | Out-File "$datapath/log-$logDate.log" -Append
 
 # Remove files older than 60 days
-Get-ChildItem –Path "$path/data/" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-60))} | Remove-Item | Out-File "$path/data/log-$logDate.log" -Append
+Get-ChildItem â€“Path "$datapath/" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-60))} | Remove-Item | Out-File "$datapath/log-$logDate.log" -Append
